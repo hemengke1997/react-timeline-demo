6ab15afa37733b51e41b51477b5908a1eb39a128
hemengke <23536175@qq.com>
2021-06-01 11:18:06 +0800

    update

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   base: '/hemengke1997.github.io',
-  publicPath: '/hemengke1997.github.io/',
+  // publicPath: '/hemengke1997.github.io/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   // more config: https://d.umijs.org/config
 });
